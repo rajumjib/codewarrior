b92015a390074d72fba48c232d478c63a01f7b97
Jahirul Islam Bhuiyan <0nline4help@gmail.com>
2014-05-25 22:52:00 +0600

    Revert "Revert "Sample Model has been added""
    This reverts commit b6f4a53d9a6d224994d18421f97d93d58219a18e.

---
+++ CodeWarrior/CodeWarrior.Business.Model/Entities/Answer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarrior.Business.Model
+{
+    public partial class Answer
+    {
+        public virtual int Id
+        {
+            get;
+            set;
+        }
+
+        public virtual int QuestionId
+        {
+            get { return _questionId; }
+            set
+            {
+                if (_questionId != value)
+                {
+                    if (Question != null && Question.Id != value)
+                    {
+                        Question = null;
+                    }
+                    _questionId = value;
+                }
+            }
+        }
+        private int _questionId;
+
+        public virtual string AnswerText
+        {
+            get;
+            set;
+        }
+
+        public virtual int Votes
+        {
+            get;
+            set;
+        }
+
+        public virtual System.DateTime DateCreated
+        {
+            get;
+            set;
+        }
+
+        public virtual Question Question
+        {
+            get;
+            set;
+        }
+
+        public virtual ICollection<Comment> Comments
+        {
+            get
+            {
+                if (_comments == null)
+                {
+                    _comments = new List<Comment>();
+                }
+                return _comments;
+            }
+            set
+            {
+                _comments = value;
+            }
+        }
+        private ICollection<Comment> _comments;
+    }
+}
---
+++ CodeWarrior/CodeWarrior.Business.Model/Entities/QuestionTag.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarrior.Business.Model
+{
+    public partial class QuestionTag
+    {
+        public virtual int Id
+        {
+            get;
+            set;
+        }
+
+        public virtual int TagId
+        {
+            get { return _tagId; }
+            set
+            {
+                if (_tagId != value)
+                {
+                    if (Tag != null && Tag.Id != value)
+                    {
+                        Tag = null;
+                    }
+                    _tagId = value;
+                }
+            }
+        }
+        private int _tagId;
+
+        public virtual int QuestionId
+        {
+            get { return _questionId; }
+            set
+            {
+                if (_questionId != value)
+                {
+                    if (Question != null && Question.Id != value)
+                    {
+                        Question = null;
+                    }
+                    _questionId = value;
+                }
+            }
+        }
+        private int _questionId;
+
+        public virtual Question Question
+        {
+            get;
+            set;
+        }
+
+        public virtual Tag Tag
+        {
+            get;
+            set;
+        }
+    }
+}
