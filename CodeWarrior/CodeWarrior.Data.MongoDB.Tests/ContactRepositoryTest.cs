0847b91a0577dc0f3d250b22f63f414b5f2fe9cd
Jahirul Islam Bhuiyan <0nline4help@gmail.com>
2014-05-25 07:26:00 +0600

    Sample Mongo Repository and Testing Added

---
+++ CodeWarrior/CodeWarrior.Data.MongoDB.Tests/ContactRepositoryTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
+
+namespace CodeWarrior.Data.MongoDB.Tests
+{
+    [TestClass]
+    public class ContactRepositoryTest
+    {
+        ContactRepository repository = new ContactRepository();
+
+        [TestMethod]
+        [Fact]
+        public void GetAllTest()
+        {
+            
+            var data = repository.GetAll();
+
+            //Xunit.Assert.NotNull(data);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotNull(data);
+
+            //Xunit.Assert.True(data.Count() > 0);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsTrue(data.Count() > 0);
+
+        }
+
+        [TestMethod]
+        [Fact]
+        public void SaveTest()
+        {
+            var contact = new Contact
+            {
+                Name = "Jahirul Islam",
+                Address = "Dhaka",
+                Email = "raju_mjib@yahoo.com",
+                Phone = "+8801914292121"
+            };
+
+            var data = repository.Save(contact);
+
+            //Xunit.Assert.NotNull(data);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotNull(data);
+        }
+
+        [Fact]
+        public void DivideByZeroThrowsException()
+        {
+            Xunit.Assert.Throws<System.DivideByZeroException>(
+                delegate
+                {
+                    DivideNumbers(5, 0);
+                });
+        }
+
+        public int DivideNumbers(int theTop, int theBottom)
+        {
+            return theTop / theBottom;
+        }
+
+        [Fact(Timeout = 50)]
+        public void TestThatRunsTooLong()
+        {
+            System.Threading.Thread.Sleep(250);
+        }
+
+        [Fact(Skip = "Can't figure out where this is going wrong...")]
+        public void BadMath()
+        {
+            Xunit.Assert.Equal(5, 2 + 2);
+        }
+    }
+}
---
+++ CodeWarrior/CodeWarrior.Data.MongoDB/ContactRepository.cs
@@ -0,0 +1,91 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarrior.Data.MongoDB
+{
+
+    public class ContactRepository
+    {
+        readonly MongoDatabase mongoDatabase;
+
+        public ContactRepository()
+        {
+            mongoDatabase = RetreiveMongohqDb();
+        }
+
+        private MongoDatabase RetreiveMongohqDb()
+        {
+            MongoClient mongoClient = new MongoClient(
+                new MongoUrl(ConfigurationManager.ConnectionStrings["MongoHQ"].ConnectionString));
+            MongoServer server = mongoClient.GetServer();
+            return mongoClient.GetServer().GetDatabase("codewarrior");
+        }
+
+
+        public IEnumerable<Contact> GetAll()
+        {
+            List<Contact> model = new List<Contact>();
+            var contactsList = mongoDatabase.GetCollection("Contacts").FindAll().AsEnumerable();
+            model = (from contact in contactsList
+                     select new Contact
+                     {
+                         Id = contact["_id"].AsString,
+                         Name = contact["Name"].AsString,
+                         Address = contact["Address"].AsString,
+                         Phone = contact["Phone"].AsString,
+                         Email = contact["Email"].AsString
+                     }).ToList();
+            return model;
+        }
+
+        public Contact Save(Contact contact)
+        {
+            var contactsList = mongoDatabase.GetCollection("Contacts");
+            WriteConcernResult result;
+            bool hasError = false;
+            if (string.IsNullOrEmpty(contact.Id))
+            {
+                contact.Id = ObjectId.GenerateNewId().ToString();
+                result = contactsList.Insert<Contact>(contact);
+                hasError = result.HasLastErrorMessage;
+            }
+            else
+            {
+                IMongoQuery query = Query.EQ("_id", contact.Id);
+                IMongoUpdate update = Update
+                    .Set("Name", contact.Name)
+                    .Set("Address", contact.Address)
+                    .Set("Phone", contact.Phone)
+                    .Set("Email", contact.Email);
+                result = contactsList.Update(query, update);
+                hasError = result.HasLastErrorMessage;
+            }
+            if (!hasError)
+            {
+                return contact;
+            }
+            else
+            {
+                throw new Exception("");
+            }
+        }
+    }
+    public class Contact
+    {
+        [BsonId]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+
+}
